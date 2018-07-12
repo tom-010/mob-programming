543bcfe367d404f3f23365b095adf34206cf276b
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2018-07-12 20:48:40 +0200

    ende mob-programming 20180712

--- mob_programming_20180712/simple_config.py
+++ mob_programming_20180712/simple_config.py
@@ -1,3 +1,13 @@
-class SimpleConfig:
-    def __init__(self):
-        self.params = {}
+def parse_ini_string(ini_string):
+    if not ini_string:
+        return {}
+    if "=" not in ini_string:
+        return{}
+    result = {None: {}}
+    for line in ini_string.split("\n"):
+        if '=' not in line:
+            continue
+
+        key, value = line.split('=', 1)
+        result[None][key.lstrip()] = value
+    return result
