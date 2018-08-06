ee733a042835c02c0509bd6a46462efa8c33c14d
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2018-08-06 19:18:51 +0200

    mobprogramming 20180726

--- mob_programming_20180712/simple_config.py
+++ mob_programming_20180712/simple_config.py
@@ -1,13 +1,34 @@
 def parse_ini_string(ini_string):
-    if not ini_string:
+    if not is_valid_ini(ini_string):
         return {}
-    if "=" not in ini_string:
-        return{}
     result = {None: {}}
-    for line in ini_string.split("\n"):
-        if '=' not in line:
-            continue
+    lines = filter(is_valid_line, map(str.strip, ini_string.split("\n")))
 
+    for line in lines:
+        if is_valid_section_header(line) :
+            return {}
         key, value = line.split('=', 1)
-        result[None][key.lstrip()] = value
-    return result
+        result[None][key.strip()] = value.strip()
+    return result if not result == {None: {}} else {} 
+
+def is_valid_line(line):
+    return len(line) > 0 and (is_valid_property(line) or is_valid_section_header(line)) 
+
+def is_valid_property(line):
+    return line[0] != '=' and '=' in line
+
+def is_valid_ini(ini_string):
+    return ini_string
+
+def is_valid_section_header(line):
+    return line[0] == "["
+
+# Nächste Schritte:
+# Das Problem ist, dass wir noch keine Abstraktion haben, die Sektionen beschreibt.
+# Gewollt ist, dass, sobald ein invalid Sektion-Header gegeben ist, die komplette Sektion gedroppt wird.
+# Idee als wir aufgehört haben: Einen Regex auf dem ini_string Sektionen erkennen lassen, diesen string mit dem
+# Regex splitten, überprüfen, ob er (einzige) Key in der Sektion valid ist. 
+# Falls nein: Ein continue in der for-loop
+# Falls ja: Wie oben die einzelnen lines verarbeiten
+# Achtung: TDD in kleinen Schritten!
+# www.regex101.com
--- mob_programming_20180712/test_simple_config.py
+++ mob_programming_20180712/test_simple_config.py
@@ -6,40 +6,50 @@ from simple_config import parse_ini_string
     ("", {}),
     (" ", {}),
     ("\n", {}),
-    (" \n      \n    \n\n", {})
+    (" \n      \n    \n\n", {}),
+    ("\n\n\n\t\n\n\t\n", {})
 ])
-def parse_empty_edge_cases(test_input, expected):
+def test_parse_empty_edge_cases(test_input, expected):
     assertParse(test_input, expected)
 
 def test_parse_global_option():
     assert parse_ini_string("global_option=true") == {
         None: {'global_option': 'true'}}
 
-
 def test_parse_global_option_without_value_and_equal():
     assert parse_ini_string("global_option=") == {
         None: {'global_option': ""}}
 
-
-@pytest.mark.parametrize("test_input,expected", [
-    ("global_option", {}),
-    ("=val", {})
-])
-def test_parse_invalid_input(test_input, expected):
-    assertParse(test_input, expected)
-
-
 @pytest.mark.parametrize("test_input,expected", [
     ("global_option=true", {None: {"global_option": "true"}}),
-    ("global_option=", {None: {"global_option": ""}}),
-    ("global_option=\n", {None: {"global_option": ""}}),
-    ("\nglobal_option=", {None: {"global_option": ""}}),
-    ("  global=val", {None: {"global": "val"}}),
-    ("global=a=b", {None: {"global": "a=b"}})
 ])
 def test_parse_single_global_options(test_input, expected):
     assertParse(test_input, expected)
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("global_option=", {None: {"global_option": ""}}),
+    ("global_option=\n", {None: {"global_option": ""}}),
+    ("\nglobal_option=", {None: {"global_option": ""}}),
+])
+def test_key_without_value(test_input, expected):
+    assertParse(test_input, expected)
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("  global=val", {None: {"global": "val"}}),
+    ("global =val", {None: {"global": "val"}}),
+    ("global= val", {None: {"global": "val"}}),
+    ("global=    val   ", {None: {"global": "val"}}),
+])
+def test_lines_with_whitespace(test_input, expected):
+    assertParse(test_input, expected)
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("global=a=b", {None: {"global": "a=b"}}),
+    ("global=a=b=c=d=e", {None: {"global": "a=b=c=d=e"}}),
+    ("global==", {None: {"global": "="}})
+])
+def test_multiple_equals(test_input, expected):
+    assertParse(test_input, expected)
 
 @pytest.mark.parametrize("test_input,expected", [
     ("global_option1=true\nglobal_option2=false", {
@@ -48,11 +58,35 @@ def test_parse_single_global_options(test_input, expected):
 def test_parse_multiple_global_options(test_input, expected):
     assertParse(test_input, expected)
 
-
 def assertParse(ini_string, expected):
     assert parse_ini_string(ini_string) == expected
 
+@pytest.mark.parametrize("test_input,expected", [
+    ("global_option", {}),
+    ("=val", {}),
+    ("=val2", {})
+])
+def test_parse_invalid_input(test_input, expected):
+    assertParse(test_input, expected)
 
-# leerzeichen vor key, nach key, vor value, nach value
-# Section without keys and
-# =value
+def test_emtpy_section():
+    assertParse("[]", {})
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("[]\na=b", {}),
+    ("""
+    []
+    a=b
+    """, {}),
+    ("[] \n a=b", {}),
+    ("[ \n a=b", {}),
+    ("] \n a=b", {}),
+])
+def test_invalid_section(test_input, expected):
+      assertParse(test_input, expected)
+
+
+# Empty Section
+# Section without keys but with value
+# Section without value but with key
+# Validate Section with regex
